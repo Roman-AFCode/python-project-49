3fd3fdab43ef7e1dd2f63c352b5b94e682320bf8
Roman Ko <roman_ko@gmail.com>
2022-11-28 17:14:13 +0300

    new files brain_even.py and even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python3
 
-import random
-from brain_games.cli import welcome_user
+#Вывод должен получиться следующий:
+#
+# brain-even
+#
+# Welcome to the Brain Games!
+# May I have your name? Sam
+# Hello, Sam!
+# Answer "yes" if the number is even, otherwise answer "no".
+# Question: 15
+# Your answer: no
+# Correct!
+# Question: 6
+# Your answer: yes
+# Correct!
+# Question: 7
+# Your answer: no
+# Correct!
+# Congratulations, Sam!
+
+from brain_games.scripts.even import welcome_user, parity_check
 
 
-name = welcome_user()
+def main():
+    welcome_user()
+    parity_check()
 
-def parity_check():
-    even_answer = 'yes'
-    odd_answer = 'no'
-    counter = 0
 
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    while counter != 3:
-        num = random.randint(1, 99)
-        print('Question:', num)
-        player_answer = input('Your answer: ')
-        if num % 2 == 0 and player_answer.lower() == 'yes':
-            print('Correct!')
-            counter += 1
-        elif num % 2 == 1 and player_answer.lower() == 'no':
-            print('Correct!')
-            counter += 1
-        elif num % 2 == 1 and player_answer.lower() == 'yes':
-            print(f"'{player_answer}' is wrong answer ;(. Correct answer was '{odd_answer}'. \nLet's try again, {name}!")
-            break
-        elif num % 2 == 0 and player_answer.lower() == 'no':
-            print(f"'{player_answer}' is wrong answer ;(. Correct answer was '{even_answer}'. \nLet's try again, {name}!")
-            break
-        else:
-            print('The answer n, u or dskjfkl is not correct! \nAnswer "yes" if the number is even, otherwise answer "no".')
-            break
-    if counter == 3:
-        print(f'Congratulations, {name}!')
+if __name__ == '__main__':
+    main()

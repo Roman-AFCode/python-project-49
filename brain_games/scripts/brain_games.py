49a2fe7a3d0925510712052f28588f6b43daef57
Roman Ko <roman_ko@gmail.com>
2022-11-28 17:11:18 +0300

    update cli.py and brain_games.py no longer used in the project

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 from brain_games.cli import welcome_user
-from brain_games.scripts.brain_even import parity_check
 
 
 def main():
     print('Welcome to the Brain Games!')
-    name = welcome_user()
+    welcome_user()
 
 if __name__ == '__main__':
     main()

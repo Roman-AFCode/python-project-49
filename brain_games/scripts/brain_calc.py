63d15d0f88817d75e19269b4586cf5bdb3569857
Roman Ko <roman_ko@gmail.com>
2022-12-06 17:31:03 +0300

    a script was created brain_calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games_engine import result_check
+from brain_games.games import calc
+
+
+def main():
+    result_check(calc)
+
+
+if __name__ == '__main__':
+    main()

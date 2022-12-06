0fd901dcb4ceca61845010daac3461aebc43d639
Roman Ko <roman_ko@gmail.com>
2022-12-06 17:34:05 +0300

    updated even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,8 +2,10 @@
 from random import randint
 
 
+print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
 def game_function():
-    print('Answer "yes" if the number is even, otherwise answer "no".')
     random_number = randint(1, 99)
     if random_number % 2 == 0:
         result = 'yes'

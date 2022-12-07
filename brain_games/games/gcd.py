002776ed51c1e65fb5e095b971181b9be34913bf
Roman Ko <roman_ko@gmail.com>
2022-12-07 13:26:58 +0300

    added new game gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,17 @@
+# игра наибольший общий делитель
+
+from random import randint
+
+
+print('Find the greatest common divisor of given numbers.')
+
+
+def game_function():
+    result = 0
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    question = str(f'Question: {num_1} {num_2}')
+    while num_2 > 0:
+        num_1, num_2 = num_2, num_1 % num_2
+        result = num_1
+    return str(result), question

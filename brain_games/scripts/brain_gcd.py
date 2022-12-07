605db8342e2e539eb6f6701179f02bf749b8b4e0
Roman Ko <roman_ko@gmail.com>
2022-12-07 09:46:57 +0300

    added new script brain_gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games_engine import result_check
+from brain_games.games import gcd
+
+
+def main():
+    result_check(gcd)
+
+
+if __name__ == '__main__':
+    main()

6f0020f3710b31f4908d27a76498bd209cb1b0ff
Roman Ko <roman_ko@gmail.com>
2022-12-09 16:27:50 +0300

    created a script brain_prime

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games_engine import result_check
+from brain_games.games import prime
+
+
+def main():
+    result_check(prime)
+
+
+if __name__ == '__main__':
+    main()

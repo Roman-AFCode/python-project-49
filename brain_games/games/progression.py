2618ca61928f36f3ad5996d3e6126a6247d6e199
Roman Ko <roman_ko@gmail.com>
2022-12-11 10:46:54 +0300

    Removed the quotation marks error progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,9 +8,9 @@ print('What number is missing in the progression?')
 def game_function():
     random_len = randint(5, 10)
     random_step = randint(1, 5)
-    lst = [x for x in range(1, 100, random_step)][:random_len]
+    lst = [str(i) for i in range(1, 100, random_step)][:random_len]
     random_integer = randint(0, len(lst) - 1)
     result = (lst[random_integer])
     lst[random_integer] = '..'
-    question = str(f'Question: {lst}')
-    return str(result), question
+    question = ('Question: ' + (' '.join(lst)))
+    return result, question

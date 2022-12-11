3360b1120b974d70d742774368b2a36e2c8a8e4d
Roman Ko <roman_ko@gmail.com>
2022-12-11 11:21:21 +0300

    rewrote the game logic prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,12 +8,12 @@ print('Answer "yes" if given number is prime. Otherwise answer "no".')
 def game_function():
     random_number = randint(1, 100)
     question = str(f'Question: {random_number}')
-    if random_number > 1:
-        for index in range(2, random_number):
-            if (random_number % index) == 0:
-                result = 'no'
-            else:
-                result = 'yes'
+    counter = 0
+    for i in range(2, random_number // 2 + 1):
+        if (random_number % i == 0):
+            counter += 1
+    if (counter <= 0):
+        result = 'yes'
     else:
         result = 'no'
     return result, question

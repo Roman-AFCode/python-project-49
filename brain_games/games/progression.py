42a98e58f87438f09ee3fff301bc8693d20cdf2a
Roman Ko <roman_ko@gmail.com>
2022-12-14 22:22:40 +0300

    logic rewritten progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,18 +2,20 @@
 from random import randint
 
 
-GAME_RULES = 'What number is missing in the progression?'
+GAME_RULE = 'What number is missing in the progression?'
 
 
-def get_game_result():
+def progression_generation():
     random_len = randint(5, 10)
     random_step = randint(1, 5)
     lst = [str(i) for i in range(1, 100, random_step)][:random_len]
+    return lst
 
-    def result_return():
-        random_integer = randint(0, len(lst) - 1)
-        result = (lst[random_integer])
-        lst[random_integer] = '..'
-        question = ('Question: ' + (' '.join(lst)))
-        return result, question
-    return result_return()
+
+def get_game_result():
+    lst = progression_generation()
+    random_integer = randint(0, len(lst) - 1)
+    result = (lst[random_integer])
+    lst[random_integer] = '..'
+    question = ('Question: ' + (' '.join(lst)))
+    return result, question

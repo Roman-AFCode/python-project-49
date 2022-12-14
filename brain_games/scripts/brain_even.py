ac435a7f0c2f4c26101ea8ab1f5bf142e835e149
Roman Ko <roman_ko@gmail.com>
2022-12-14 21:02:58 +0300

    updated all scripts

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games_engine import result_check
+from brain_games.games_engine import start_game
 from brain_games.games import even
 
 
 def main():
-    result_check(even)
+    start_game(even)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games_engine import result_check
+from brain_games.games_engine import start_game
 from brain_games.games import prime
 
 
 def main():
-    result_check(prime)
+    start_game(prime)
 
 
 if __name__ == '__main__':
